ab914dd5f38152be8710b91a80206ef007ff3d73
Stepan Bechynsky <stepan@bechynsky.cz>
2015-02-28 14:09:50 +0000

    Script is ready to run using Cron

--- Device/ReadTemperature.py
+++ Device/ReadTemperature.py
@@ -1,10 +1,15 @@
-import os
 import glob
 import time
+import urllib2
+import RequestSignature
 
 base_dir = '/sys/bus/w1/devices/'
 device_folder = glob.glob(base_dir + '28*')[0]
 device_file = device_folder + '/w1_slave'
+
+secret = 'cbc07838-bf40-11e4-9d58-b5a7297fd676'
+device_id = 'k-palecku-1'
+url='http://raspi-temperature-01.appspot.com/save?did={0}&t={1}&sig={2}'
 
 def read_temp_raw():
 	f = open(device_file, 'r')
@@ -28,6 +33,8 @@ def read_temp():
 	
 	return temperature
 
-while True:
-	print(read_temp())
-	time.sleep(1)
+
+temperature = str(read_temp())
+signature = RequestSignature.RequestSignature.sign([device_id, temperature], secret)
+response = urllib2.urlopen(url.format(device_id, temperature, signature))
+print temperature
